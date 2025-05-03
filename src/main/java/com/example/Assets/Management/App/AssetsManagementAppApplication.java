bc15f1cd00638cca12b9a44e8b47b714e320c706
Vinay repo <vinay.git.repo@gmail.com>
2025-05-03 19:40:14 +0530

    Initilize Project

---
+++ src/main/java/com/example/Assets/Management/App/AssetsManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Assets.Management.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Assets/Management/App/AssetsManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assets.Management.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
